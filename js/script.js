62ebe3f92ce8201c2acd3831d95010ac7e891319
Sarah Morrow <smorrow72@gmail.com>
2019-02-04 16:10:51 -0500

    Added Moves Mode
    Added moves mode, and a new css file to label types to match how they look in-game.

--- js/script.js
+++ js/script.js
@@ -13,6 +13,8 @@ $(function () {
     var pokemonByName = "https://pokeapi.co/api/v2/pokemon/";
     var locationsUrl = "https://pokeapi.co/api/v2/location";
     var abilUrl = "https://pokeapi.co/api/v2/ability";
+    var movesUrl = "https://pokeapi.co/api/v2/move?limit=800";
+    var singleMoveUrl = "https://pokeapi.co/api/v2/move";
 
     var selectedOption = $(".selectedOption");
     var selectedNav = $(".selectedNav");
@@ -20,6 +22,7 @@ $(function () {
     $(".navBtn").click(function () {
         selectedNav.toggleClass("selectedNav");
         $(this).toggleClass("selectedNav");
+        selectedNav = $(this);
 
         if ($(this).attr("id") == "pokemonBtn") {
             pokemonMode();
@@ -62,6 +65,8 @@ $(function () {
 
     function movesMode() {
         clearControls();
+        getMovesList(movesUrl);
+        appendTypeBtns();      
     }
 
     function locationsMode() {
@@ -90,6 +95,56 @@ $(function () {
         });
 
         btn.appendTo(optionsDiv);
+    }
+
+    function appendTypeBtns() {
+        
+        var typeLink = "https://pokeapi.co/api/v2/type";
+
+        $.getJSON(typeLink).done(function (data) {
+            $.each(data.results, function (index, type) {
+                var typeName = type.name;
+
+                var btn = $("<button class ='type-btn " + typeName + "'>").html(typeName.charAt(0).toUpperCase() + typeName.slice(1));
+
+                btn.click(function () {
+                    var singleTypeLink = "https://pokeapi.co/api/v2/type/" + typeName;
+
+                    listDiv.empty();
+
+                    $.getJSON(singleTypeLink).done(function (typeData) {
+                        $.each(typeData.moves, function (index, move) {
+                            var name = move.name.charAt(0).toUpperCase() + move.name.slice(1);
+                            var link = $("<a>").attr("id", move.name).attr("href", "#").append($("<strong>").text(name));
+                            var par = $("<p>").append(link);
+
+                            par.appendTo(listDiv);
+
+                            link.click(getMovesDetails);
+                        });
+
+                    }).fail(function () {
+                        console.log("Request to PokeApi failed.");
+                    });
+                });
+
+                btn.appendTo(optionsDiv);
+
+            }).fail(function () {
+                console.log("Request to PokeApi failed.");
+            });
+
+        }).fail(function () {
+            console.log("Request to PokeApi failed.");
+        }); 
+
+        var allBtn = $("<button class='opt_btn'>").html("All Types");
+
+        allBtn.click(function () {
+            getMovesList(movesUrl);
+        });
+        optionsDiv.append(allBtn);
+
     }
 
     function appendTitleCol(text) {
@@ -123,6 +178,7 @@ $(function () {
         });
     }
 
+
     function getLocationList(locationsUrl) {
         $.getJSON(locationsUrl).done(function (data) {
             console.log(data);
@@ -135,6 +191,28 @@ $(function () {
             console.log("Request to PokeApi failed.");
         }).always(function () {
             console.log("Pokemon is Sweeet!");
+        });
+    }
+
+    function getMovesList(movesUrl){
+        $.getJSON(movesUrl).done(function (data) {
+            //console.log(data);
+            listDiv.empty();
+
+            $.each(data.results, function (index, move) {
+                var name = move.name.charAt(0).toUpperCase() + move.name.slice(1);
+                var link = $("<a>").attr("id", move.name).attr("href", "#").append($("<strong>").text(name));
+                var par = $("<p>").append(link);
+
+                par.appendTo(listDiv);
+
+                link.click(getMovesDetails);
+            });
+
+        }).fail(function () {
+            console.log("Request to PokeApi failed.");
+        }).always(function () {
+
         });
     }
 
@@ -184,7 +262,7 @@ $(function () {
             var types = details.types;
             types.forEach(function (item) {
                 var typeName = item.type.name;
-                typeCol.append("<p>" + typeName.charAt(0).toUpperCase() + typeName.slice(1) + " ");
+                typeCol.append("<p class='type-label " + typeName + "'>" + typeName.charAt(0).toUpperCase() + typeName.slice(1) + " ");
             });
 
             //abilCol.append("<p><strong>Abilities: </strong></p>");
@@ -204,25 +282,75 @@ $(function () {
             heightCol.appendTo(infoRow);
             weightCol.appendTo(infoRow);
 
-            infoDiv.append("<h2>Moves</h2>");
+            //infoDiv.append("<h2>Moves</h2>");
 
             // GENERATE MOVES LIST
 
-            var movesList = $("<ul>").css("id", "movesList");
+            //var movesList = $("<ul>").css("id", "movesList");
 
-            var moves = details.moves;
-            moves.forEach(function (item) {
-                var moveName = item.move.name;
-                movesList.append("<li>" + moveName.charAt(0).toUpperCase() + moveName.slice(1) + " ");
-            });
+            //var moves = details.moves;
+            //moves.forEach(function (item) {
+            //    var moveName = item.move.name;
+            //    movesList.append("<li>" + moveName.charAt(0).toUpperCase() + moveName.slice(1) + " ");
+            //});
 
-            movesList.appendTo(infoDiv);
+            //movesList.appendTo(infoDiv);
 
             infoDiv.delay(400).fadeTo(400, 1);
         });
         event.preventDefault();
     }
-    
+
+    function getMovesDetails(){
+        var name = $(this).attr("id");
+        $.getJSON(singleMoveUrl + "/" + name).done(function (details) {
+            console.log(details);
+
+            pokemonDiv.empty();
+            pokemonDiv.css("opacity", 0);
+            infoDiv.css("opacity", 0);
+
+            pokemonDiv.append("<h1>" + name.charAt(0).toUpperCase() + name.slice(1) + "</h1>");
+            var typeName = details.type.name;
+            pokemonDiv.append("<p class='type-label " + typeName + "' style='margin-top:2px;'>" + typeName.charAt(0).toUpperCase() + typeName.slice(1) + " ");
+            pokemonDiv.append("</br> </br>");
+            var description = details.flavor_text_entries[2].flavor_text;
+            pokemonDiv.append("<h3>Description: ");
+            pokemonDiv.append("<p>" + description + "</p>");
+            pokemonDiv.delay(100).fadeTo(400, 1);
+
+            titleRow.empty();
+            infoRow.empty();
+
+            appendTitleCol("Power");
+            appendTitleCol("PP");
+            appendTitleCol("Damage Class");
+            appendTitleCol("Contest Type");
+
+
+            var powerCol = $("<td>").attr("id", "powerCol").attr("class", "infoCol");
+            var ppCol = $("<td>").attr("id", "ppCol").attr("class", "infoCol");
+            var damageCol = $("<td>").attr("id", "damageCol").attr("class", "infoCol");
+            var contestCol = $("<td>").attr("id", "contestCol").attr("class", "infoCol");
+            
+            powerCol.append("<p>" + details.power + "</p>");
+            ppCol.append("<p>" + details.pp + "</p>");
+            var damageName = details.damage_class.name;
+            damageCol.append("<p>" + damageName.charAt(0).toUpperCase() + damageName.slice(1) + "</p>");
+            var contestName = details.contest_type.name;
+            contestCol.append("<p>" + contestName.charAt(0).toUpperCase() + contestName.slice(1) + "</p>");
+
+            infoRow.append(powerCol);
+            infoRow.append(ppCol);
+            infoRow.append(damageCol);
+            infoRow.append(contestCol);
+
+            
+            infoDiv.delay(400).fadeTo(400, 1);
+
+        });
+        event.preventDefault();
+    }
 
 
 });
